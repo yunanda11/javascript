78560f9bd52c10ae7fb3ee100913108bac201dd2
yunanda11 <adithazard21@gmail.com>
2024-10-01 19:16:18 +0700

    tugas2

---
+++ tugas2.js
@@ -0,0 +1,19 @@
+var number=21;
+var string="abcde";
+var boolean="false";
+cm=180;
+
+if(cm>170)
+    boolean="true";
+else
+boolean="false";
+var uund;
+var aray=[1,2,3,4,5];
+
+
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(uund);
+console.log(aray[2]);
