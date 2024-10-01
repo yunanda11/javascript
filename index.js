29366f1de140a9db1bdf4e5d124679cae106a269
yunanda11 <adithazard21@gmail.com>
2024-10-01 18:44:27 +0700

    menambah file index dan tugas1

---
+++ index.js
@@ -0,0 +1,5 @@
+var message = "hello Coding";
+let msg1 ="hello";
+const msg2 ="Coding"
+
+console.log(msg2)
---
+++ tugas1.js
@@ -0,0 +1,7 @@
+var ndepan="arvi";
+let nbelakang="angga";
+const umur =27
+
+console.log(ndepan);
+console.log(nbelakang);
+console.log(umur)
